876bcb12b86019bcc9956765194058c66d00480c
Giygas <gustavorossich@gmail.com>
2023-01-12 02:43:02 +0100

    Changed all the file structure for production

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Glightbox options
-const lightbox = GLightbox({
-  'href': 'https://www.youtube.com/watch?v=qL-Qe7bJopY',
-  'type': 'video',
-  'source': 'youtube', //vimeo, youtube or local
-  'width': 900,
-  'autoplayVideos': true,
-  'touchNavigation': true
-});
