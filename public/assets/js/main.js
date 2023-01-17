568682f821b0dcde04a0468f645702e7d7c2816f
Giygas <gustavorossich@gmail.com>
2023-01-17 23:39:04 +0100

    fix: wait for css to load before showing the page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,3 +8,10 @@ const lightbox = GLightbox({
   'autoplayVideos': true,
   'touchNavigation': true
 })
+
+// WAIT FOR CSS TO LOAD
+var stylesheet = document.getElementById("stylesheet")
+
+stylesheet.onload = function() {
+  document.getElementById("page").style.display = "block"
+}
